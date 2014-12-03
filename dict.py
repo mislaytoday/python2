3c4e139c4d6ce3026847487ef777a90b00ab36aa
sayid <mislaytoday@gmail.com>
2014-12-03 16:57:34 +0800

    python base syntax

---
+++ dict.py
@@ -0,0 +1,4 @@
+d={'Micheal':80,'Bob':85,'Sayid':25}
+print 'Micheal Score=', d['Micheal']
+print 'Sayid Score=',d['Sayid']
+print d
---
+++ ifelse.py
@@ -0,0 +1,7 @@
+age = int(raw_input("enter you age:"))
+if age >= 18:
+  print 'you are old'
+elif age >=6:
+  print 'you are child'
+else:
+  print 'you are kid'
