023483d91c5be13105a0782f1c54c778dd12f7cd
Sayid <mislaytoday@gmail.com>
2014-12-10 16:05:30 +0800

    add slice demo,string slice ,tuple slice

---
+++ day2/slicedemo1.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print 'L = ',L
+# 从index0开始取值,到索引3为止,不包括3
+print 'L[0:3]',L[0:3]
+# 如果index从 0 开始,可以省略 
+print 'L[:3]',L[:3]
+# 倒数第一个元素的索引为 -1 ,第二个为 -2 依此类推
+print 'L[-1]',L[-1]
+print 'L[-2]',L[-2]
+# 前10个数,每两个取一个
+print 'L[:10:2]',L[:10:2]
---
+++ day2/stringslice.py
@@ -0,0 +1,5 @@
+str = 'ABCDEFG'
+print str
+print 'str[:3]',str[:3]
+print 'str[::2]',str[::2]
+
